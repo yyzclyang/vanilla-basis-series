3d1af024ab6bc78f2bb83a954ac838ee40c820e7
yyzcl <yyzclyang@outlook.com>
2020-09-22 00:07:10 +0800

    feat(eventHub): 实现 eventHub 的 on 和 emit

--- src/eventHub/__tests__/eventHub.test.ts
+++ src/eventHub/__tests__/eventHub.test.ts
@@ -1,8 +1,33 @@
 import EventHub from '../';
 
+let eventHub: EventHub;
+
+beforeEach(() => {
+  eventHub = new EventHub();
+});
+
 describe('EventHub', () => {
   test('是一个类', () => {
     expect(EventHub).toBeInstanceOf(Function);
     expect(EventHub.prototype).toBeInstanceOf(Object);
+  });
+  test('eventHub 能订阅一个事件之后，通过 emit 触发它', () => {
+    const fn = jest.fn();
+    eventHub.on('test-event', fn);
+    eventHub.emit('test-event');
+    expect(fn).toBeCalled();
+  });
+  test('eventHub 能订阅一个事件之后，通过 emit 触发它并传入一个参数，这个参数会被订阅事件的函数调用时使用', () => {
+    const fn = jest.fn();
+    eventHub.on('test-event', fn);
+    eventHub.emit('test-event', 'event-data');
+    expect(fn).toBeCalled();
+    expect(fn).toBeCalledWith('event-data');
+  });
+  test('eventHub 需先订阅再触发', () => {
+    const fn = jest.fn();
+    eventHub.emit('test-event');
+    eventHub.on('test-event', fn);
+    expect(fn).not.toBeCalled();
   });
 });
--- src/eventHub/eventHub.ts
+++ src/eventHub/eventHub.ts
@@ -1,3 +1,22 @@
-class EventHub {}
+interface EventList {
+  [key: string]: Array<(eventData: unknown) => void>;
+}
+
+class EventHub {
+  private eventList: EventList = {};
+
+  // 订阅
+  on(eventName: string, fn: (eventData: unknown) => void) {
+    this.eventList[eventName] = this.eventList[eventName] ?? [];
+    this.eventList[eventName].push(fn);
+  }
+  // 发布
+  emit(eventName: string, eventData?: unknown) {
+    if (this.eventList[eventName] === undefined) {
+      return;
+    }
+    this.eventList[eventName].forEach((fn) => fn(eventData));
+  }
+}
 
 export default EventHub;
