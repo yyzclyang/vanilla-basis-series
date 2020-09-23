f53aa62da33f05e93d734dca35d68c4624e52d38
yyzcl <yyzclyang@outlook.com>
2020-09-24 00:43:49 +0800

    feat(bind): 支持调用时再传入参数

--- src/bind/__tests__/bind.test.ts
+++ src/bind/__tests__/bind.test.ts
@@ -5,7 +5,7 @@ describe('BIND', () => {
     expect(BIND).toBeInstanceOf(Function);
     expect(BIND.prototype).toBeInstanceOf(Object);
   });
-  test('能固定 this', () => {
+  test('能绑定 this', () => {
     const fn = function() {
       return this;
     };
@@ -16,7 +16,7 @@ describe('BIND', () => {
     expect(bindFn1() === thisArg1).toBeTrue();
     expect(bindFn2() === thisArg2).toBeTrue();
   });
-  test('支持同时传入参数', () => {
+  test('支持绑定 this 的同时传入参数', () => {
     const fn = function(arg1: any, arg2: any) {
       return [this, arg1, arg2];
     };
@@ -25,6 +25,19 @@ describe('BIND', () => {
     const arg2 = {};
     const bindFn = fn.BIND(thisArg, arg1, arg2);
     const [r1, r2, r3] = bindFn();
+    expect(r1 === thisArg).toBeTrue();
+    expect(r2 === arg1).toBeTrue();
+    expect(r3 === arg2).toBeTrue();
+  });
+  test('支持绑定 this 时传入一部分参数，调用时传入另外一部分参数', () => {
+    const fn = function(arg1: any, arg2: any) {
+      return [this, arg1, arg2];
+    };
+    const thisArg = 123;
+    const arg1 = [];
+    const arg2 = {};
+    const bindFn = fn.BIND(thisArg, arg1);
+    const [r1, r2, r3] = bindFn(arg2);
     expect(r1 === thisArg).toBeTrue();
     expect(r2 === arg1).toBeTrue();
     expect(r3 === arg2).toBeTrue();
--- src/bind/bind.ts
+++ src/bind/bind.ts
@@ -1,8 +1,8 @@
 function BIND(thisArg: any, ...argArray: any[]) {
   // this 即调用 bind 的函数
   const thisFn = this;
-  return function() {
-    return thisFn.apply(thisArg, argArray);
+  return function(...argArray2: any[]) {
+    return thisFn.apply(thisArg, argArray.concat(argArray2));
   };
 }
 
