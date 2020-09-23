b0773a2b45a54616444a017fe7800e19994a5a58
yyzcl <yyzclyang@outlook.com>
2020-09-24 00:25:16 +0800

    feat(bind): 实现 bind 绑定 this 的同时能传入参数

--- src/bind/__tests__/bind.test.ts
+++ src/bind/__tests__/bind.test.ts
@@ -16,4 +16,17 @@ describe('BIND', () => {
     expect(bindFn1() === thisArg1).toBeTrue();
     expect(bindFn2() === thisArg2).toBeTrue();
   });
+  test('支持同时传入参数', () => {
+    const fn = function(arg1: any, arg2: any) {
+      return [this, arg1, arg2];
+    };
+    const thisArg = 123;
+    const arg1 = [];
+    const arg2 = {};
+    const bindFn = fn.BIND(thisArg, arg1, arg2);
+    const [r1, r2, r3] = bindFn();
+    expect(r1 === thisArg).toBeTrue();
+    expect(r2 === arg1).toBeTrue();
+    expect(r3 === arg2).toBeTrue();
+  });
 });
--- src/bind/bind.ts
+++ src/bind/bind.ts
@@ -1,8 +1,8 @@
-function BIND(thisArg) {
+function BIND(thisArg: any, ...argArray: any[]) {
   // this 即调用 bind 的函数
   const thisFn = this;
   return function() {
-    return thisFn.apply(thisArg);
+    return thisFn.apply(thisArg, argArray);
   };
 }
 
